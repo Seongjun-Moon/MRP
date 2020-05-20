c2dbee797eb3476f78bd0af9e48eeee7992ea4cd
Seongjun Moon <moonsj1128@gmail.com>
2020-05-20 14:42:20 +0900

    router

--- nodeServer/routers/hospitalRouter.js
+++ nodeServer/routers/hospitalRouter.js
@@ -1,1 +1,10 @@
 // 병원 라우터
+const express=require('express');
+const router=express.Router();
+
+// 의약품 유통이력 조회
+router.post('search', (res,req)=>{
+
+})
+
+module.exports=router;
--- nodeServer/routers/overseeRouter.js
+++ nodeServer/routers/overseeRouter.js
@@ -1,1 +1,26 @@
 //심평원 라우터
+const express=require('express');
+const router=express.Router();
+const MediInfo=require('../models').MediInfo;
+
+// 의약품 유통이력 조회
+router.post('search', (res,req)=>{
+
+})
+
+// 의약품 정보 조회
+router.post("mediInfo", async(req, res)=>{
+    try{
+        const mediInfoAll=await MediInfo.findAll({
+
+        })
+
+        console.log(mediInfoAll);
+        res.json(mediInfoAll);
+    }catch(err){
+        console.log(err);
+        res.json({message:false})
+    }
+})
+
+module.exports=router;

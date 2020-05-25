de96722882d3afc5ef47a40cc6d231bee2a616a5
mincheol-creator <kimmc1030@naver.com>
2020-05-25 13:17:14 +0900

    회원가입 로그인 유저페이지 작업중

--- web/src/API.js
+++ web/src/API.js
@@ -8,6 +8,10 @@ const getMedicineInfo = () => {
   return axios.post(`http://${url}/oversee/mediInfo`, {});
 };
 
+/* const getUerInfo = () => {
+  return axios.post(`http://${url}/oversee/userInfo`, {});
+}; */
+
 const createCompanyInfo = (companyCode, companyName, companyType) => {
   return axios.post(`http://${url}/manufacture/companyEnroll`, {
     companyCode,
@@ -20,8 +24,27 @@ const getCompanyInfo = () => {
   return axios.post(`http://${url}/manufacture/companyInfo`, {});
 };
 
+const getCustomer = (email, password, section) => {
+  return axios.post(`http://${url}/`, {
+    email,
+    password,
+    section,
+  });
+};
+
+const addCustomer = (email, password, section) => {
+  return axios.post(`http://${url}/`, {
+    email,
+    password,
+    section,
+  });
+};
+
 export default {
   getMedicineInfo,
   createCompanyInfo,
   getCompanyInfo,
+  getCustomer,
+  addCustomer,
+  /* getUerInfo, */
 };

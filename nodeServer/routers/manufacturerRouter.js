2bf4e8423c2f94078ced49b14e2d73b7010e1d05
Seongjun-Moon <moonsj1128@gmail.com>
2020-05-26 11:43:04 +0900

    수정 : 업체정보 조회, 의약품 정보 조회 코드 수정 company table, medicine table에서 불러올 때 특정 값만 불러오도록 수정

--- nodeServer/routers/manufacturerRouter.js
+++ nodeServer/routers/manufacturerRouter.js
@@ -1,48 +1,46 @@
 //제조사 라우터
-const express=require('express');
-const router=express.Router();
-const company=require('../models').Company;
+const express = require("express");
+const router = express.Router();
+const company = require("../models").Company;
 
 // 의약품 유통이력 조회
-router.post('/search', (res,req)=>{
-
-})
+router.post("/search", (res, req) => {});
 
 // 업체 등록
-router.post("/companyEnroll", async(req,res)=>{
-    const companyCode=req.body.companyCode
-    const companyName=req.body.companyName
-    const companyType=req.body.companyType
-    
-    try{
-        const companyEnroll=await company.create({
-            companyCode,
-            companyName,
-            companyType
-        });
+router.post("/companyEnroll", async (req, res) => {
+  const companyCode = req.body.companyCode;
+  const companyName = req.body.companyName;
+  const companyType = req.body.companyType;
 
-        console.log(companyEnroll);
-        res.json({message:true});
+  try {
+    const companyEnroll = await company.create({
+      companyCode,
+      companyName,
+      companyType,
+    });
 
-    }catch(err){
-        console.log(err);
-        res.json({message:false});
-    }
-})
+    console.log(companyEnroll);
+    res.json({ message: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
 
 // 업체 정보 조회
-router.post("/companyInfo", async(req, res)=>{
-    try{
-        const companyInfo=await company.findAll({
+router.post("/companyInfo", async (req, res) => {
+  try {
+    const companyInfo = await company.findAll({
+      attributes: ["companyCode", "companyName", "companyType"],
+      order: [["companyCode", "ASC"]],
+    });
 
-        })
+    console.log(companyInfo);
+    res.json(companyInfo);
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
 
-        console.log(companyInfo);
-        res.json(companyInfo);
-    }catch(err){
-        console.log(err);
-        res.json({message:false});
-    }
-})
-
-module.exports=router;
+module.exports = router;
--- nodeServer/routers/overseeRouter.js
+++ nodeServer/routers/overseeRouter.js
@@ -1,76 +1,80 @@
 //심평원 라우터
-const express=require('express');
-const router=express.Router();
-const medicine=require('../models').Medicine;
-const company=require('../models').Company;
+const express = require("express");
+const router = express.Router();
+const medicine = require("../models").Medicine;
+const company = require("../models").Company;
 
 // 의약품 유통이력 조회
-router.post('/search', (res,req)=>{
-
-})
+router.post("/search", (res, req) => {});
 
 // 의약품 정보 등록
-router.post("/mediEnroll", async(req,res)=>{
-    const mediCode=req.body.mediCode
-    const companyCode=req.body.companyCode
-    const mediName=req.body.mediName
-    const mediType=req.body.mediType
-    const count=req.body.count
-    const permissionDate=req.body.permissionDate
-    const cancelDate=req.body.cancelDate
+router.post("/mediEnroll", async (req, res) => {
+  const mediCode = req.body.mediCode;
+  const companyCode = req.body.companyCode;
+  const mediName = req.body.mediName;
+  const mediType = req.body.mediType;
+  const count = req.body.count;
+  const permissionDate = req.body.permissionDate;
+  const cancelDate = req.body.cancelDate;
 
-    
-    try{
-        const mediEnroll=await medicine.create({
-            mediCode,
-            companyCode,
-            mediName,
-            mediType,
-            count,
-            permissionDate,
-            cancelDate
-        });
+  try {
+    const mediEnroll = await medicine.create({
+      mediCode,
+      companyCode,
+      mediName,
+      mediType,
+      count,
+      permissionDate,
+      cancelDate,
+    });
 
-        console.log(mediEnroll);
-        res.json({message:true});
-
-    }catch(err){
-        console.log(err);
-        res.json({message:false});
-    }
-})
+    console.log(mediEnroll);
+    res.json({ message: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
 
 // 의약품 정보 조회
-router.post("/mediInfo", async(req, res)=>{
-    try{
-        const mediInfo=await medicine.findAll({
-            
-            
-            order: [['permissionDate', 'DESC']]
-        });
+router.post("/mediInfo", async (req, res) => {
+  try {
+    const mediInfo = await medicine.findAll({
+      attributes: [
+        "mediCode",
+        "companyCode",
+        "mediName",
+        "mediType",
+        "count",
+        "permissionDate",
+        "cancelDate",
+      ],
 
-        console.log(mediInfo);
-        res.json(mediInfo);
-    }catch(err){
-        console.log(err);
-        res.json({message:false})
-    }
-})
+      order: [["permissionDate", "DESC"]],
+    });
+
+    console.log(mediInfo);
+    res.json(mediInfo);
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
 
 // 업체 정보 조회
-router.post("/companyInfo", async(req, res)=>{
-    try{
-        const companyInfo=await company.findAll({
-            
-            order: [['companyCode', 'ASC']]
-        })
+router.post("/companyInfo", async (req, res) => {
+  try {
+    const companyInfo = await company.findAll({
+      attributes: ["companyCode", "companyName", "companyType"],
+      order: [["companyCode", "ASC"]],
+    });
 
-        console.log(companyInfo);
-        res.json(companyInfo);
-    }catch(err){
-        console.log(err);
-        res.json({message:false})
-    }
-})
+    console.log(companyInfo);
+    res.json(companyInfo);
+  } catch (err) {
+    console.log(err);
+    res.json({ message: false });
+  }
+});
 
-module.exports=router;
+module.exports = router;

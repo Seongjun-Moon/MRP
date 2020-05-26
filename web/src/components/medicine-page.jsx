185353c04e812cb43b2ce2cb670aaef9f40c27e7
kkoomin <kkooming2@gmail.com>
2020-05-26 14:53:15 +0900

    Fix components: separate list and register form

--- web/src/components/medicine-page.jsx
+++ web/src/components/medicine-page.jsx
@@ -62,111 +62,38 @@ function MedicinePage(props) {
   };
 
   return (
-    <section className="medicine">
-      <article className="medicine-list">
-        <h3>의약품 목록 조회</h3>
-        <table className="medicine-list_table">
-          <thead>
-            <tr>
-              <td className="mediCode">의약품 코드</td>
-              <td className="companyCode">업체 코드</td>
-              <td className="mediName">의약품 이름</td>
-              <td className="mediType">의약품 종류</td>
-              <td className="count">갯수</td>
-              <td className="permissionDate">허가 일자</td>
-              <td className="cancelDate">취소 일자</td>
-            </tr>
-          </thead>
-          <tbody>
-            {mediData.map((medi) => {
-              return (
-                <tr key={medi.mediCode}>
-                  <td>{medi.mediCode}</td>
-                  <td>{medi.companyCode}</td>
-                  <td>{medi.mediName}</td>
-                  <td>{medi.mediType}</td>
-                  <td>{medi.count}</td>
-                  <td>{medi.permissionDate}</td>
-                  <td>{medi.cancelDate}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </article>
-
-      <article>
-        <h3>의약품 정보 등록</h3>
-        <form action="" className="medicine-form" onSubmit={createMedicineInfo}>
-          <label htmlFor="mediCode">의약품 표준코드</label>
-          <input
-            type="text"
-            name=""
-            id="mediCode"
-            ref={(ref) => (mediCode = ref)}
-            required
-          />
-
-          <label htmlFor="mediName">의약품명</label>
-          <input
-            type="text"
-            name=""
-            id="mediName"
-            ref={(ref) => (mediName = ref)}
-            required
-          />
-
-          <label htmlFor="companyCode">제조업체 코드</label>
-          <input
-            type="text"
-            name=""
-            id="companyCode"
-            ref={(ref) => (companyCode = ref)}
-            required
-          />
-
-          <label htmlFor="mediType">의약품 유형</label>
-          <select
-            name=""
-            id="mediType"
-            ref={(ref) => (mediType = ref)}
-            required
-          >
-            <option value="의약품">전문 의약품</option>
-          </select>
-
-          <label htmlFor="count">제품 수량</label>
-          <input
-            type="number"
-            name=""
-            id="count"
-            ref={(ref) => (count = ref)}
-            required
-          />
-
-          <label htmlFor="permissionDate">품목 허가일자</label>
-          <input
-            type="date"
-            name=""
-            id="permissionDate"
-            ref={(ref) => (permissionDate = ref)}
-            required
-          />
-
-          <label htmlFor="cancelDate">취소일자</label>
-          <input
-            type="date"
-            name=""
-            id="cancelDate"
-            ref={(ref) => (cancelDate = ref)}
-            required
-          />
-          <button type="submit" className="main-btn">
-            등록
-          </button>
-        </form>
-      </article>
-    </section>
+    <article className="medicine-list">
+      <h3>의약품 목록 조회</h3>
+      <input type="text" placeholder="의약품명 검색" />
+      <table className="medicine-list_table">
+        <thead>
+          <tr>
+            <td className="mediCode">의약품 코드</td>
+            <td className="companyCode">업체 코드</td>
+            <td className="mediName">의약품 이름</td>
+            <td className="mediType">의약품 종류</td>
+            <td className="count">갯수</td>
+            <td className="permissionDate">허가 일자</td>
+            <td className="cancelDate">취소 일자</td>
+          </tr>
+        </thead>
+        <tbody>
+          {mediData.map((medi) => {
+            return (
+              <tr key={medi.mediCode}>
+                <td>{medi.mediCode}</td>
+                <td>{medi.companyCode}</td>
+                <td>{medi.mediName}</td>
+                <td>{medi.mediType}</td>
+                <td>{medi.count}</td>
+                <td>{medi.permissionDate}</td>
+                <td>{medi.cancelDate}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </article>
   );
 }
 
--- web/src/components/sidebar-container.jsx
+++ web/src/components/sidebar-container.jsx
@@ -11,10 +11,16 @@ function SidebarContainer(props) {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/company">업체 페이지</NavLink>
+          <NavLink to="/company">업체 목록 조회</NavLink>
         </li>
         <li>
-          <NavLink to="/medicine">전문 의약품 페이지</NavLink>
+          <NavLink to="/company/register">업체 정보 등록</NavLink>
+        </li>
+        <li>
+          <NavLink to="/medicine">전문의약품 목록 조회</NavLink>
+        </li>
+        <li>
+          <NavLink to="/medicine/register">전문의약품 정보 등록</NavLink>
         </li>
         <li>
           <NavLink to="/circulation">유통 페이지</NavLink>

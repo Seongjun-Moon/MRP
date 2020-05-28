a8765befa34774a8f3b16a3ff906850806813f89
Seongjun-Moon <moonsj1128@gmail.com>
2020-05-28 14:46:42 +0900

    기능 :  검증 코드 추가 및 수정 입/출고 등록시 대상 companyCode가 등록이 되어 있어야만 제출 가능 입/출고 등록 시 barcode에 있는 mediCode를 검색하여 등록되어 있는 mediCode만 제출 가능 회원가입 시 companyCode  검증하는 코드 수정

--- nodeServer/controllers/distributorController.js
+++ nodeServer/controllers/distributorController.js
@@ -1,30 +1,44 @@
 const company = require("../models").Company;
 const temp = require("../models").Temp;
+const medicine = require("../models").Medicine;
 
 // 입,출고 등록
 const productEnroll = async (req, res) => {
-  const barcode = req.body.data.barcode;
+  const barcode = String(req.body.data.barcode);
   const companyCode = req.body.data.companyCode;
   const targetCompanyCode = req.body.data.targetCompanyCode;
   const state = req.body.data.state;
 
   try {
     const getCompanyCode = await company.findOne({
-      attributes: ["companyCode"],
+      where: { companyCode: targetCompanyCode },
     });
     if (!getCompanyCode) {
       res.json({ message: false });
     } else {
       try {
-        const productEnroll = await temp.create({
-          barcode,
-          companyCode,
-          targetCompanyCode,
-          state,
+        const mediCode = barcode.substring(4, 17); /// barcode 내에서 medicode 뽑아내기
+        const barcodeVerify = await medicine.findOne({
+          where: { mediCode },
         });
+        if (!barcodeVerify) {
+          res.json({ message: false });
+        } else {
+          try {
+            const productEnroll = await temp.create({
+              barcode,
+              companyCode,
+              targetCompanyCode,
+              state,
+            });
 
-        console.log(productEnroll);
-        res.json({ message: true, productEnroll: productEnroll });
+            console.log(productEnroll);
+            res.json({ message: true, productEnroll: productEnroll });
+          } catch (err) {
+            console.log(err);
+            res.json({ message: false });
+          }
+        }
       } catch (err) {
         console.log(err);
         res.json({ message: false });

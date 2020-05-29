bfdf62f3520bfaf01c7eb9fab98e792d67830c75
jinah92 <jinah0818@gmail.com>
2020-05-28 22:38:12 -0700

    update chaincode_sample file

--- nodeServer/controllers/chaincode_sample.js
+++ nodeServer/controllers/chaincode_sample.js
@@ -13,8 +13,8 @@ const Barcode = require("../models").Barcode;
 const ccpPath = path.resolve(
   __dirname,
   "..",
-  "basic-2org-network",
-  "connection-org1.json"
+  "basic-2org-network",		// .env 파일로 이동
+  "connection-org1.json"	// 세션에 있는 변수값으로 받음
 );
 const ccpJSON = fs.readFileSync(ccpPath, "utf8");
 const ccp = JSON.parse(ccpJSON);
@@ -28,13 +28,12 @@ const walletPath = path.join(process.cwd(), "wallet");
 const wallet = new FileSystemWallet(walletPath);
 
 // 체인코드
-const chainCode = "jihwan2";
+const chainCode = "CHAINCODE_NAME";	// .env 파일로 이동
 
 // 채널
-const channel = "mychannel";
+const channel = "CHANNEL_NAME";		// .env 파일로 이동
 
 // ==================  GET Method ==================
-
 // 1. MRP 블록체인 네트워크 연결 시도
 const connect = async (req, res) => {
   try {
@@ -102,66 +101,10 @@ const connect = async (req, res) => {
   }
 };
 
-// 2. 모든 전문의약품의 최신 유통정보 조회
-const queryAll = async (req, res) => {
-  let dataArr = [];
-  const data = req.body.data;
-  // 표준코드로 바코드 찾아오기
-  // try {
-  //   const medicineData = await Barcode.findAll({
-  //     attributes: ["barcodeName"],
-  //     include: {
-  //       model: Medicine,
-  //       attributes: ["mediCode"],
-  //       where: {
-  //         data,
-  //       },
-  //     },
-  //   });
-  //   console.log(medicineData);
-  //   medicineData.ForEach((e) => {
-  //     dataArr.push(e);
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  // }
-
-  try {
-    const userExists = await wallet.exists("user1");
-    if (!userExists) {
-      console.log(
-        'An identity for the user "user1" does not exist in the wallet'
-      );
-      await res.json({ msg: "연결부터 해주세요" });
-      return;
-    }
-    console.log("Start : Qeury All Medicine Info");
-    // Create a new gateway for connecting to our peer node.
-    const gateway = new Gateway();
-    await gateway.connect(ccp, {
-      wallet,
-      identity: "user1",
-      discovery: { enabled: false },
-    });
-    // Get the network (channel) our contract is deployed to.
-    const network = await gateway.getNetwork(channel);
-
-    // Get the contract from the network.
-    const contract = network.getContract(chainCode);
-
-    const result = await contract.evaluateTransaction("showAll", "");
-    console.log(
-      `Transaction has been evaluated, result is: ${result.toString()}`
-    );
-    console.log("End : Qeury All Medicine Info");
-    res.json({ allInfo: result.toString() });
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 // ==================  POST Method ==================
-// 3. 전문의약품 유통정보 신규등록 (제조)
+// 2. 전문의약품 유통정보 신규등록 (제조)
+// front-end에서 barcode, companyCode, targetCompanyCode, state 순으로 인자 전달
 const register = async (req, res) => {
   try {
     const userExists = await wallet.exists("user1");
@@ -205,7 +148,9 @@ const register = async (req, res) => {
   }
 };
 
-// 4. 전문의약품 유통정보 등록 (도매, 병원 및 약국)
+// 3. 전문의약품 유통정보 등록 (도매, 병원 및 약국)
+// 기존 블록체인에 등록된 경우에만 정상 수행함
+// front-end에서 barcode, companyCode, targetCompanyCode, state 순으로 인자 전달
 const update = async (req, res) => {
   try {
     const userExists = await wallet.exists("user1");
@@ -245,11 +190,114 @@ const update = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.json({ code: "0", msg: `${req.body.barcode} 입력 오류` });
+    res.json({ code: "0", msg: `${req.body.barcode} 기존 유통내역이 존재하지 않습니다.` });
   }
 };
 
-// 5. 특정 전문의약품의 유통 히스토리를 조회
+// 4. 단일 전문의약품의 현재 유통정보를 확인 (world state)
+// front-end에서 barcode 1개를 인자로 전달
+const getBarcode = async (req, res) => {
+    try {
+    const userExists = await wallet.exists('user1');
+    if (!userExists) {
+      console.log(
+        'An identity for the user "user1" does not exist in the wallet'
+      );
+      await res.json({ msg: '연결부터 해주세요' });
+      return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: 'user1',
+      discovery: { enabled: false },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork(channel);
+
+    // Get the contract from the network.
+    const contract = network.getContract(chainCode);
+
+    const result = await contract.evaluateTransaction(
+      'getBarcode',
+      `${req.body.barcode}`
+    );
+    const state = JSON.parse(result);
+    res.json({state});
+
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+
+// 5. 표준코드에 대응하는 모든 바코드 항목을 조회
+// front-end에서 mediCode(표준코드) 1개를 인자로 전달
+const barcodeList = async (req, res) => {
+  try {
+    
+    const arr = [];
+    const barcodeList = await Barcode.findAll({
+      attributes: ["barcodeName", "mediCode"],
+      where: {
+        mediCode: req.body.mediCode,
+      },
+    });
+    // 대응되는 바코드 개수 (size)
+    const size = Object.keys(barcodeList).length;
+    for (let i = 0; i < size; i++) {
+      console.log("barcord " + i + ":" + barcodeList[i].barcodeName);
+      arr.push(barcodeList[i].barcodeName);
+      console.log(barcodeList[i].barcodeName);
+    }
+    // 콜백함수 (블록체인 네트워크에 바코드별 최신 유통정보 조회)
+    // showBarcodes([바코드 배열]);
+
+    //res.json({ arr });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// 6. 각 바코드의 현재 유통상태 조회 (world state)
+// ??? 에서 [barcode1,barcode2,...,]를 인자로 전달
+const showBarcodes = async(req, res)=> {
+   try{
+    const mediCode = req.body;	// 바코드 배열
+    console.log(mediCode);
+    console.log(mediCode.toString());
+
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: "user1",
+      discovery: { enabled: false },
+    });
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork(channel);
+
+    // Get the contract from the network.
+    const contract = network.getContract(chainCode);
+
+    const result = await contract.evaluateTransaction(
+      "getAllBarcode",
+      mediCode.toString()
+    ); 
+    console.log(result);
+    const state = JSON.parse(result);
+    res.json({state});
+   }catch (err) {
+    console.log(err);
+   }
+
+}); 
+
+// 7. 특정 전문의약품의 유통 히스토리를 조회
+// front-end에서 barcode 인자(1개) 전달
 const history = async (req, res) => {
   try {
     const userExists = await wallet.exists("user1");
@@ -286,52 +334,5 @@ const history = async (req, res) => {
   }
 };
 
-// 6. 표준코드에 대응하는 모든 바코드 항목을 조회
-const barcodeList = async (req, res) => {
-  try {
-    
-    const arr = [];
-    const barcodeList = await Barcode.findAll({
-      attributes: ["barcodeName", "mediCode"],
-      where: {
-        mediCode: req.body.mediCode,
-      },
-    });
-
-    // 대응되는 바코드 개수 (size)
-    const size = Object.keys(barcodeList).length;
-    for (let i = 0; i < size; i++) {
-      console.log("barcord " + i + ":" + barcodeList[i].barcodeName);
-      arr.push(barcodeList[i].barcodeName);
-      console.log(barcodeList[i].barcodeName);
-    }
-    /*
-    const sendString = JSON.stringify(arr);
-    console.log(sendString);
-
-    const gateway = new Gateway();
-    await gateway.connect(ccp, {
-      wallet,
-      identity: "user1",
-      discovery: { enabled: false },
-    });
-    // Get the network (channel) our contract is deployed to.
-    const network = await gateway.getNetwork(channel);
-
-    // Get the contract from the network.
-    const contract = network.getContract(chainCode);
-
-    const result = await contract.evaluateTransaction(
-      "showByKeyArray",
-      sendString
-    ); 
-     
-    console.log(result);
-    */
-    res.json({ arr });
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 module.exports = { connect, queryAll, register, update, history, barcodeList };
